e254bee50b7b84c212c3747b78ca8fe4eaf30188
Johana Pérez García <131737154+Johana3010@users.noreply.github.com>
2023-05-06 14:41:16 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('<span style="color:#3576c6"</span>Desarrolladora de software y promotora de la participación juvenil</span>')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
